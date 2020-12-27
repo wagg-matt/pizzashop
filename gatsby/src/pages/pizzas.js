91e6e1ed88170d05c563219fb8d6afc564997dad
Matt Wagg <matt@mattwagg.com>
2020-12-27 12:17:51 +0000

    pizza list styling

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-export default function PizzasPage() {
+import PizzaList from '../components/PizzaList';
+
+export default function PizzasPage({ data }) {
+  console.log('props: ', data);
+  const pizzas = data.pizzas.nodes;
   return (
     <>
-      <p>This is the pizzas page</p>
+      <PizzaList pizzas={pizzas} />
     </>
   );
 }
+
+export const query = graphql`
+  query {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
